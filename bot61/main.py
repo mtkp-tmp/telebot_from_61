6ab1bbb7bfb978186dbdee40da4ac078c9344777
Satan <31803702+Surdum@users.noreply.github.com>
2018-03-25 01:51:36 +0300

    Update main.py

--- bot61/main.py
+++ bot61/main.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
+import sys
+sys.path.insert(0, "./search_bar")
+
 import telebot
+import search_bar
 
 token = "LaLaLaLaLa"
 bot = telebot.TeleBot(token)
@@ -7,8 +11,8 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(commands=['Start'])
 def on_message(message):
   bot.send_message(message.chat.id, 'Здравствуйте! Я совместо создание группы\n'
-                                     'ТМП-61 bot61 v0.0. Что я умею делать:\n'
-                                     '-- blablabla')
+                                     'ТМП-61 bot61 v1.0. Что я умею делать:\n'
+                                     '- Находить ближайший бар')
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)

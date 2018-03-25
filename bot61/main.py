a073371ab9b5012f55ceb71af1adacd76bc4d3b7
Satan <31803702+Surdum@users.noreply.github.com>
2018-03-25 13:10:13 +0300

    Update main.py

--- bot61/main.py
+++ bot61/main.py
@@ -11,7 +11,7 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(commands=['Start'])
 def on_message(message):
   bot.send_message(message.chat.id, 'Здравствуйте! Я совместо создание группы\n'
-                                     'ТМП-61 bot61 v1.0. Что я умею делать:\n'
+                                     'ТМП-61 bot61 v1.1. Что я умею делать:\n'
                                      '- Находить ближайший бар')
 
 if __name__ == "__main__":
